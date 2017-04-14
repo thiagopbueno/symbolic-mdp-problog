a25eb76d7443e47ee74063f1c5d9394a873ded6d
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-04-14 13:38:34 -0300

    Add WeightedDNF class

---
+++ symbolic-mdp-problog/tests/test_wdnf.py
@@ -0,0 +1,94 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+
+import unittest
+import parser
+import wdnf
+
+
+class TestWeightedDNF(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.parser = parser.Parser()
+
+        filename1 = os.path.join(os.path.dirname(__file__), 'models/value.pl')
+        with open(filename1, 'r') as input:
+            cls.value = input.read()
+        cls.atoms1, cls.rules1 = cls.parser.parse(cls.value)
+
+        filename2 = os.path.join(os.path.dirname(__file__), 'models/transition.pl')
+        with open(filename2, 'r') as input:
+            cls.transition = input.read()
+        cls.atoms2, cls.rules2 = cls.parser.parse(cls.transition)
+
+    def test_add_term(self):
+        # value function
+        wdnf1 = wdnf.WeightedDNF(self.atoms1)
+        self.assertEqual(len(wdnf1), 0)
+        for index, rule in enumerate(self.rules1):
+            wdnf1.add_term(rule.body, int(rule.head[2:-1]))
+            self.assertEqual(len(wdnf1), index + 1)
+
+        fluents = ['running(c1,1)', 'running(c2,1)', 'running(c3,1)']
+        for fluent in fluents:
+            pos = ('+', fluent)
+            neg = ('-', fluent)
+            t1 = set(wdnf1.terms_by_literals([pos]))
+            t2 = set(wdnf1.terms_by_literals([neg]))
+            self.assertEqual(len(t1), len(wdnf1) / 2)
+            self.assertEqual(len(t2), len(wdnf1) / 2)
+            self.assertEqual(t1 & t2, set())
+            self.assertEqual(t1 | t2, set(wdnf1.terms))
+
+        values = [0, 1, 2, 3]
+        terms_sets = [ set(wdnf1.terms_by_weight(v)) for v in values ]
+        self.assertEqual(sum([len(s) for s in terms_sets]), len(wdnf1))
+        for i in range(len(terms_sets[-1])):
+            for j in range(i + 1, len(terms_sets)):
+                self.assertEqual(terms_sets[i] & terms_sets[j], set())
+
+        # transition function
+        wdnf2 = wdnf.WeightedDNF(self.atoms2)
+        self.assertEqual(len(wdnf2), 0)
+        for index, rule in enumerate(self.rules2):
+            pos_term = tuple([('+', rule.head)] + list(rule.body))
+            wdnf2.add_term(pos_term, rule.probability)
+            neg_term = tuple([('-', rule.head)] + list(rule.body))
+            wdnf2.add_term(neg_term, 1 - rule.probability)
+        self.assertEqual(len(wdnf2), 2 * len(self.rules2))
+
+        next_state_fluents = ['running(c1,1)', 'running(c2,1)', 'running(c3,1)']
+        for fluent in next_state_fluents:
+            pos = ('+', fluent)
+            neg = ('-', fluent)
+            t1 = set(wdnf2.terms_by_literals([pos]))
+            t2 = set(wdnf2.terms_by_literals([neg]))
+            self.assertEqual(len(t1), len(t2))
+
+            inter = set(term for _, term in t1 & t2)
+            self.assertTrue(pos not in inter)
+            self.assertTrue(neg not in inter)
+
+        actions = ['reboot(c1)', 'reboot(c2)', 'reboot(c3)']
+        for action in actions:
+            pos = ('+', action)
+            neg = ('-', action)
+            t1 = set(wdnf2.terms_by_literals([pos]))
+            t2 = set(wdnf2.terms_by_literals([neg]))
+            inter = set(term for _, term in t1 & t2)
+            self.assertTrue(pos not in inter)
+            self.assertTrue(neg not in inter)
+
+            if action == actions[0]:
+                self.assertEqual(len(t1), len(wdnf2) - 6)
+                self.assertEqual(len(t2), len(wdnf2) - 2)
+
+            if action == actions[2]:
+                self.assertEqual(len(t1), len(wdnf2) - 6)
+                self.assertEqual(len(t2), len(wdnf2) - 2)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
---
+++ symbolic-mdp-problog/wdnf.py
@@ -0,0 +1,55 @@
+class WeightedDNF():
+
+    def __init__(self, atoms):
+        self._atoms = atoms
+        self._terms = []
+        self._weights = {}
+        self.__index = {}
+        for atom in self._atoms:
+            self.__index[('+', atom)] = set()
+            self.__index[('-', atom)] = set()
+
+    def __len__(self):
+        return len(self._terms)
+
+    def __getitem__(self, index):
+        return self._terms[index]
+
+    @property
+    def terms(self):
+        return self._terms
+
+    def add_term(self, term, weight):
+        self.__update_index(term, len(self))
+        self._terms.append((weight, term))
+        self._weights[weight] = self._weights.get(weight, [])
+        self._weights[weight].append(term)
+
+    def terms_by_weight(self, weight):
+        return self._weights.get(weight, [])
+
+    def terms_by_literals(self, literals):
+        if len(literals) == 0:
+            return self._terms
+        all_terms_indexes = set(range(0, len(self._terms)))
+        consistent_list = []
+        for l in literals:
+            consistent_list.append(self.__index.get(l, all_terms_indexes))
+        consistent_list = sorted(consistent_list, key=len)
+        intersect = consistent_list[0]
+        for term_set in consistent_list[1:]:
+            intersect &= term_set
+        return [ self[index] for index in sorted(intersect) ]
+
+    def __update_index(self, term, index):
+        term = set(term)
+        for atom in self._atoms:
+            positive_literal, negative_literal = ('+', atom), ('-', atom)
+            if positive_literal in term:
+                self.__index[positive_literal].add(index)
+                continue
+            if negative_literal in term:
+                self.__index[negative_literal].add(index)
+                continue
+            self.__index[positive_literal].add(index)
+            self.__index[negative_literal].add(index)
